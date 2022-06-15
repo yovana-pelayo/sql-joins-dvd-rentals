d1e2dc5977ac4cc39f6129f0d94c770376631488
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-06-15 12:41:45 -0700

    city number found

--- exercise_1/exercise-1.sql
+++ exercise_1/exercise-1.sql
@@ -1,2 +1,5 @@
 -- find all address_id of addresses in the city of Dundee
 -- use INNER JOIN FROM address on to city
+SELECT address_id from address 
+inner join city on city.city_id= address.city_id
+where address.city_id= 149;
