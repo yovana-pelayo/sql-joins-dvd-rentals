3af77ed1995330b13740cfe85de15c6fee63688e
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-06-17 09:32:36 -0700

    dev branch

--- exercise_4/exercise-4.sql
+++ exercise_4/exercise-4.sql
@@ -1,23 +1,5 @@
--- find all customers first_name, last_name, address, and city
--- Similar to exercise 3 but with customer, address, city
--- SELECT address_id
--- FROM customer
--- LEFT JOIN address on customer.address_id = address.address_id
--- LEFT JOIN address on city.city_id = address
-
--- SELECT city.city
--- FROM city
--- LEFT JOIN address
--- ON city.city_id = address.city_id
--- LEFT JOIN customer 
--- ON customer.customer_id = customer.customer_id
--- WHERE customer= first_name, last_name, address, city
--- ;
-SELECT city.city
-FROM city
-LEFT JOIN address
-ON city.city_id = address.city_id
-LEFT JOIN customer 
-ON customer.customer_id = customer.customer_id
-WHERE customer= first_name, last_name, address, city
-;
+SELECT city.city, customer.first_name, customer.last_name, address.address
+FROM customer
+LEFT JOIN address ON customer.address_id = address.address_id
+LEFT JOIN city
+ON address.city_id = city.city_id;
